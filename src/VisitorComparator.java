6e2e13f79b025259228933bd81a42f42c4fb6403
Novice-6 <419664199@qq.com>
2025-11-28 22:53:47 +0800

    Update VisitorComparator.java
    Create a VisitorComparator and implement the Comparator interface.

--- src/VisitorComparator.java
+++ src/VisitorComparator.java
@@ -1,2 +1,18 @@
-public class VisitorComparator {
+import java.util.Comparator;
+
+/**
+ * 游客比较器，实现 Comparator 接口（Part 4B 要求）
+ * 比较逻辑：先按年龄升序，年龄相同则按会员类型字典序
+ */
+public class VisitorComparator implements Comparator<Visitor> {
+    @Override
+    public int compare(Visitor v1, Visitor v2) {
+        // 第一排序条件：年龄
+        int ageCompare = Integer.compare(v1.getAge(), v2.getAge());
+        if (ageCompare != 0) {
+            return ageCompare;
+        }
+        // 第二排序条件：会员类型（字典序）
+        return v1.getMembershipType().compareTo(v2.getMembershipType());
+    }
 }
